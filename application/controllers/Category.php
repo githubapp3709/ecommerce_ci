5c961446246e541cbc4ccdfcae1db2e041b276d8
shakti <githubapp3709@gmail.com>
2024-04-30 16:37:29 +0530

    added category form validation

--- application/controllers/Category.php
+++ application/controllers/Category.php
@@ -5,7 +5,13 @@ class Category extends CI_Controller
 {
     public function index()
     {
-
-        $this->load->view('category');
+        $this->form_validation->set_rules('cate_name', 'Category Name', 'required|trim');
+        $this->form_validation->set_rules('status', 'Status', 'required|trim');
+        if ($this->form_validation->run()) {
+            $post = $this->input->post();
+            print_r($post);
+        } else {
+            $this->load->view('category');
+        }
     }
 }
--- application/views/category.php
+++ application/views/category.php
@@ -40,44 +40,29 @@
                                 <div class="row">
                                     <div class="col-md-6">
                                         <div class="form-floating mb-3">
-                                            <input type="text" class="form-control" id="floatingFirstnameInput" placeholder="Category Name">
+                                            <select class="form-select" id="floatingSelectGrid" name="parent_id">
+                                                <option selected="">Select</option>
+                                            </select>
+                                            <label for="floatingSelectGrid">Parent Category</label>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-floating mb-3">
+                                            <input type="text" class="form-control" id="floatingFirstnameInput" placeholder="Category Name" name="cate_name">
                                             <label for="floatingFirstnameInput">Category Name</label>
                                         </div>
+                                        <?= form_error('cate_name') ?>
                                     </div>
                                     <div class="col-md-6">
                                         <div class="form-floating mb-3">
-                                            <input type="text" class="form-control" id="floatingLastnameInput" placeholder="Enter Your Last Name">
-                                            <label for="floatingLastnameInput">Last Name</label>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        <div class="form-floating mb-3">
-                                            <input type="email" class="form-control" id="floatingemailInput" placeholder="Enter Email address">
-                                            <label for="floatingemailInput">Email address</label>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <div class="form-floating mb-3">
-                                            <select class="form-select" id="floatingSelectGrid" aria-label="Floating label select example">
-                                                <option selected="">Open this select menu</option>
-                                                <option value="1">One</option>
-                                                <option value="2">Two</option>
-                                                <option value="3">Three</option>
+                                            <select class="form-select" id="floatingSelectGrid" name="status">
+                                                <option value="" selected="">Select</option>
+                                                <option value="1">Active</option>
+                                                <option value="0">Deactive</option>
                                             </select>
-                                            <label for="floatingSelectGrid">Works with selects</label>
+                                            <label for="floatingSelectGrid">Status</label>
                                         </div>
-                                    </div>
-                                </div>
-
-                                <div class="mb-3">
-
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="checkbox" id="floatingCheck">
-                                        <label class="form-check-label" for="floatingCheck">
-                                            Check me out
-                                        </label>
+                                        <?= form_error('status') ?>
                                     </div>
                                 </div>
                                 <div>
