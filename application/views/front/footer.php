cf866a4ca6a1e4224a7304585666cb2e72411d8c
shakti <githubapp3709@gmail.com>
2024-05-06 18:19:38 +0530

    added front theme

---
+++ application/views/front/footer.php
@@ -0,0 +1,16 @@
+<script src="assets_front/js/vendor/jquery.js"></script>
+<script src="assets_front/js/vendor/waypoints.js"></script>
+<script src="assets_front/js/bootstrap-bundle.js"></script>
+<script src="assets_front/js/meanmenu.js"></script>
+<script src="assets_front/js/swiper-bundle.js"></script>
+<script src="assets_front/js/slick.js"></script>
+<script src="assets_front/js/range-slider.js"></script>
+<script src="assets_front/js/magnific-popup.js"></script>
+<script src="assets_front/js/nice-select.js"></script>
+<script src="assets_front/js/purecounter.js"></script>
+<script src="assets_front/js/countdown.js"></script>
+<script src="assets_front/js/wow.js"></script>
+<script src="assets_front/js/isotope-pkgd.js"></script>
+<script src="assets_front/js/imagesloaded-pkgd.js"></script>
+<script src="assets_front/js/ajax-form.js"></script>
+<script src="assets_front/js/main.js"></script>
